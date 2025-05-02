509eaad8b5ca46f440abcdb487247a642e590d07
Alex <aprildistant@gmail.com>
2025-05-02 16:43:48 +0300

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <>
+      <h1>Приложение на React</h1>
+      <Button variant='primary'>Нажми меня</Button>
+    </>
+  );
+}
+
+export default App;
