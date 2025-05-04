a1a374c5b3bd7dc4abfcf5637472d1fda7c0c28e
Alex <aprildistant@gmail.com>
2025-05-04 19:09:18 +0300

    Реализован роутинг в http-сервере на NodeJS.

--- server/index.js
+++ server/index.js
@@ -1,21 +1,51 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = 3000;
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method)   // В запросе будет: / GET
 
-    res.setHeader('Content-Type', 'text/plain')
-    res.write('<h1>Hello from NodeJS</h1>')
-    res.write('<h2>Hello from NodeJS</h2>')
-    res.write('<h3>Hello from NodeJS</h3>')
-    res.end(`
-   <div style="background: red; width: 200px; height: 200px;">
-    <h1>Test</h1>
-   </div>
-  `)
-})
+const server = http.createServer((req, res) => {
+    console.log('Server request');
+
+    res.setHeader('Content-Type', 'text/html');
+
+    const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+    let basePath = '';
+
+    switch (req.url) {
+        case '/':
+        case '/home':
+        case '/index.html':
+            basePath = createPath('index');
+            res.statusCode = 200;
+            break;
+        case '/about-us':
+            res.statusCode = 301;
+            res.setHeader('Location', '/contacts');
+            res.end();
+            break;
+        case '/contacts':
+            basePath = createPath('contacts');
+            res.statusCode = 200;
+            break;
+        default:
+            basePath = createPath('error');
+            res.statusCode = 404;
+            break;
+    }
+
+    fs.readFile(basePath, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.statusCode = 500;
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
+});
 
 server.listen(PORT, 'localhost', (error) => {
-    console.log('Server is running...')
-    error ? console.log(error) : console.log(`Listening port  ${PORT}`);
-})
+    error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
